10b1ee7e5743bc2844c47ce447481840cc28da82
Massimo Paladin <massimo.paladin@gmail.com>
2015-12-30 15:10:18 +0100

    SONARJAVA-1273 Rule : S3346 "assert" should only be used with boolean variables

---
+++ java-checks/src/main/java/org/sonar/java/checks/AssertOnBooleanVariableCheck.java
@@ -0,0 +1,61 @@
+/*
+ * SonarQube Java
+ * Copyright (C) 2012-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.java.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.java.checks.helpers.ExpressionsHelper;
+import org.sonar.java.tag.Tag;
+import org.sonar.plugins.java.api.tree.AssertStatementTree;
+import org.sonar.plugins.java.api.tree.ExpressionTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.Tree.Kind;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+
+import java.util.List;
+
+@Rule(
+  key = "S3346",
+  name = "\"assert\" should only be used with boolean variables",
+  priority = Priority.MAJOR,
+  tags = {Tag.SUSPICIOUS})
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.INSTRUCTION_RELIABILITY)
+@SqaleConstantRemediation("5min")
+public class AssertOnBooleanVariableCheck extends SubscriptionBaseVisitor {
+
+  @Override
+  public List<Kind> nodesToVisit() {
+    return ImmutableList.of(Kind.ASSERT_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    ExpressionTree condition = ExpressionsHelper.skipParentheses(((AssertStatementTree) tree).condition());
+    if (!condition.is(Kind.IDENTIFIER, Kind.MEMBER_SELECT)) {
+      reportIssue(condition, "Move this \"assert\" side effect to another statement.");
+    }
+  }
+
+}
